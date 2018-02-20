a393e1f74bd0628070d7cc5872d9c8d0cac58727
Andrei <b1naryth1ef@gmail.com>
2018-02-19 16:25:58 -0800

    Collect go runtime stats

--- lib/punt/metrics.go
+++ lib/punt/metrics.go
@@ -2,6 +2,7 @@ package punt
 
 import (
 	"github.com/DataDog/datadog-go/statsd"
+	"github.com/bmhatfield/go-runtime-metrics/collector"
 )
 
 // New creates a new DogStatsD client.
@@ -11,4 +12,16 @@ func NewStatsdClient(prefix string, tags []string) *statsd.Client {
 	s.Namespace = prefix + "."
 	s.Tags = tags
 	return s
+}
+
+func RunRuntimeCollector() {
+	s, _ := statsd.NewBuffered("127.0.0.1:8125", 100)
+	c := collector.New(func(key string, value uint64) {
+		s.Gauge(key, float64(value), nil, 1)
+	})
+
+	c.EnableCPU = true
+	c.EnableMem = true
+	c.EnableGC = true
+	c.Run()
 }
